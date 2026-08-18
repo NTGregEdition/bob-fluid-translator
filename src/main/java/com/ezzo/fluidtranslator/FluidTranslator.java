d19324e6145348df3040e3998469603a70c6f404
Arystan_X <mrarystanx@gmail.com>
2026-08-18 16:35:43 +0500

    2.1.6 (just number)

--- src/main/java/com/ezzo/fluidtranslator/FluidTranslator.java
+++ src/main/java/com/ezzo/fluidtranslator/FluidTranslator.java
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger;
 public class FluidTranslator
 {
     public static final String MODID = "bobfluidtranslator";
-    public static final String VERSION = "2.1.1";
+    public static final String VERSION = "2.1.6";
 
     @SidedProxy(
             clientSide = "com.ezzo.fluidtranslator.ClientProxy",
